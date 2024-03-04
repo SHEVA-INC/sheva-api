cb02ce17f2d1ec1f03234af3e10e766082cf8296
Munkoor <eee1lvlboss@gmail.com>
2024-03-04 11:40:47 +0200

    created order,boot and user app

--- ShevaAPI/settings.py
+++ ShevaAPI/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "boots",
+    "order",
     "user",
 ]
 
@@ -101,6 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+AUTH_USER_MODEL = "user.User"
 
 
 # Internationalization
--- boots/models.py
+++ boots/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    COLOR_CHOICES = [
+        ('red', 'Red'),
+        ('blue', 'Blue'),
+        ('green', 'Green'),
+        ('black', 'Black'),
+        ('white', 'White'),
+        ('yellow', 'Yellow'),
+        ('purple', 'Purple'),
+        ('orange', 'Orange'),
+        ('another', 'Another'),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    color = models.CharField(max_length=100, choices=COLOR_CHOICES)
+    size = models.PositiveIntegerField()
+    stock = models.PositiveIntegerField()
+    brand = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='product_images/')
+
+    def __str__(self):
+        return self.name
+
