41fd5968142a86ba6bb27f4fab8f76b07aa72eed
Vitalii <vitalii.shtark@looqme.io>
2024-03-04 13:58:31 +0200

    Changed some fields in order models

--- order/models.py
+++ order/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 from ShevaAPI import settings
-from boots.models import Product
+from boots.models import Boots
 
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product, through='OrderItem')
+    boots = models.ManyToManyField(Boots, through='OrderItem')
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     shipping_address = models.TextField()
     order_date = models.DateTimeField(auto_now_add=True)
 
 
 class OrderItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Boots, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
