d79e40357b4090137908f80b91fe2cc40b8de578
Vitalii <vitalii.shtark@looqme.io>
2024-03-04 13:49:49 +0200

    Made small changes in ShevaApi

--- ShevaAPI/urls.py
+++ ShevaAPI/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/boots/", include("boots.urls", namespace="boots")),
+    path("api/reviews/", include("review.urls", namespace="review")),
+
 ]
