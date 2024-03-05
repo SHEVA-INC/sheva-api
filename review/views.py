ebe70226e328a68ef9c485575a9271683c283859
Munkoor <vitalii.shtark@looqme.io>
2024-03-05 12:56:18 +0200

    Added isowner permission in reviews

--- review/views.py
+++ review/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 
 from review.models import Review
+from review.permissions import IsOwnerOrReadOnly
 from review.serializers import ReviewSerializer
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [IsOwnerOrReadOnly]
