ff7a02648a4ab16e51cc7c7e8a6aec2c72116bae
Ihor Sharov <ihorsharov75@gmail.com>
2024-04-28 21:36:22 +0300

    Fixed error with user/login

--- user/urls.py
+++ user/urls.py
@@ -9,7 +9,7 @@ from user.views import CreateUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path("login/", views.CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login", views.CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("change_password/", views.ChangePasswordView.as_view(), name="change_password"),
