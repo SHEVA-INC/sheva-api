2ab11efc5e03ed3503847451659be371b12bae0e
Munkoor <eee1lvlboss@gmail.com>
2024-06-20 16:32:47 +0300

    Added additional settings with pagination

--- ShevaAPI/settings.py
+++ ShevaAPI/settings.py
@@ -141,6 +141,8 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.AnonRateThrottle',
         'rest_framework.throttling.UserRateThrottle'
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
     'DEFAULT_THROTTLE_RATES': {
         'anon': '150/day',
         'user': '1000/day'
