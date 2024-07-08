46eb811f91afdafba99c3571043b5bbaf69f7e49
Munkoor <eee1lvlboss@gmail.com>
2024-07-08 13:53:50 +0300

    Added cart_product_id in view_cart endpoint

--- cart/serializers.py
+++ cart/serializers.py
@@ -6,14 +6,16 @@ from .models import Cart, CartProduct
 
 class CartProductSerializer(serializers.ModelSerializer):
     product = BootsCartSerializer()
+    cart_product_id = serializers.IntegerField(source='pk', read_only=True)
 
     class Meta:
         model = CartProduct
-        fields = ['product', 'size', 'quantity', 'subtotal']
+        fields = ['cart_product_id', 'product', 'size', 'quantity', 'subtotal']
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         product_representation = representation.pop('product')
+        product_representation['cart_product_id'] = representation['cart_product_id']
         product_representation['quantity'] = representation['quantity']
         product_representation['subtotal'] = representation['subtotal']
         product_representation['size'] = instance.size
