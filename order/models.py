0e610eb8b54a06f6e884f22830e28fe565567705
Munkoor <eee1lvlboss@gmail.com>
2024-08-04 14:46:26 +0300

    Changed some fields in order/models.py and made a migrations

--- order/models.py
+++ order/models.py
@@ -3,19 +3,14 @@ from cart.models import Cart
 
 
 class Order(models.Model):
-    DELIVERY_CHOICES = [
-        ("self-delivery", "Самовивіз"),
-        ("delivery-man", "Кур'єр"),
-    ]
-
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=255)
     phone_number = models.IntegerField()
     email = models.EmailField()
     region = models.CharField(max_length=255)
     city_town = models.CharField(max_length=255)
-    post_office_number = models.IntegerField()
-    delivery_method = models.CharField(max_length=255, choices=DELIVERY_CHOICES)
+    post_office_number = models.CharField(max_length=255)
+    payment_method = models.CharField(max_length=255, default="Оплатити при отриманні")
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
