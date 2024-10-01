cc0f47946b45d23bfb1dd27c08992fd767441977
Serhii <serhiimelnyk11@gmail.com>
2024-10-01 20:41:48 +0300

    Added brand for filters.py

--- boots/filters.py
+++ boots/filters.py
@@ -9,10 +9,11 @@ class BootsFilter(django_filters.FilterSet):
     size = django_filters.NumberFilter(field_name="sizes__size")
     color = django_filters.ChoiceFilter(choices=Boots.COLOR_CHOICES)
     type = django_filters.ChoiceFilter(choices=Boots.TYPE_CHOICES)
+    brand = django_filters.ChoiceFilter(choices=Boots.BRAND_CHOICES)
 
     class Meta:
         model = Boots
-        fields = ['size', 'color', 'type']
+        fields = ['size', 'color', 'type', 'brand']
 
 
 class CustomPageNumberPagination(PageNumberPagination):
